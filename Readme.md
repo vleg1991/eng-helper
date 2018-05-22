92b2b35d4a304f4fd283c429b95697dc6bdffb9f
EgorovVF <EgorovVF@altarix.ru>
2018-05-22 17:24:47 +0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Back-end project for "English helper" application.
---
+++ src/main/java/com/vleg/eng/helper/EngHelperApplication.java
@@ -0,0 +1,12 @@
+package com.vleg.eng.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngHelperApplication.class, args);
+	}
+}
